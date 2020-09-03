2b249d7cc97a542770156b843300308c37e43743
Julio Di Polito Neto <julio.dipolito@tesigroup.tech>
2020-09-03 15:24:36 -0300

    Added BlockTrack.App as a xamarin project to handle iOS and Android apps

---
+++ BlockTrack.App/BlockTrack.App/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using BlockTrack.App.Models;
+
+namespace BlockTrack.App.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ BlockTrack.App/BlockTrack.App/Views/MenuPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using BlockTrack.App.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BlockTrack.App.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MenuPage : ContentPage
+    {
+        MainPage RootPage { get => Application.Current.MainPage as MainPage; }
+        List<HomeMenuItem> menuItems;
+        public MenuPage()
+        {
+            InitializeComponent();
+
+            menuItems = new List<HomeMenuItem>
+            {
+                new HomeMenuItem {Id = MenuItemType.Browse, Title="Browse" },
+                new HomeMenuItem {Id = MenuItemType.About, Title="About" }
+            };
+
+            ListViewMenu.ItemsSource = menuItems;
+
+            ListViewMenu.SelectedItem = menuItems[0];
+            ListViewMenu.ItemSelected += async (sender, e) =>
+            {
+                if (e.SelectedItem == null)
+                    return;
+
+                var id = (int)((HomeMenuItem)e.SelectedItem).Id;
+                await RootPage.NavigateFromMenu(id);
+            };
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Sorry","Not implemented yet :-C", "OK");
+        }
+    }
+}
